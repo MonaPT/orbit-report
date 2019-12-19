36e498141c56f8411ff2e3df2fba28020a3557c6
Mona <Mona@Srikanths-MacBook-Pro-2.local>
2019-12-18 18:42:30 -0600

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Satellite } from './satellite';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,58 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'orbit-report';
+
+  sourceList: Satellite[];
+  /* First task - With Arrays - BEGIN
+  constructor() {
+    this.sourceList = [
+       new Satellite("SiriusXM", "Communication", "2009-03-21", "LOW", true),
+       new Satellite("Cat Scanner", "Imaging", "2012-01-05", "LOW", true),
+       new Satellite("Weber Grill", "Space Debris", "1996-03-25", "HIGH", false),
+       new Satellite("GPS 938", "Positioning", "2001-11-01", "HIGH", true),
+       new Satellite("ISS", "Space Station", "1998-11-20", "LOW", true),
+    ];
+ }*/ //First task - With Arrays - END
+
+ //Second task - with JSON - BEGIN
+
+ constructor() {
+  this.sourceList = [];
+  let satellitesUrl = 'https://handlers.education.launchcode.org/static/satellites.json';
+  let satellite;
+  this.displayList = [];
+
+  window.fetch(satellitesUrl).then(function(response) {
+     response.json().then(function(data) {
+
+        let fetchedSatellites = data.satellites;
+        for(let i=0; i<fetchedSatellites.length; i++){
+          satellite= new Satellite(fetchedSatellites[i].name, 
+             fetchedSatellites[i].type, fetchedSatellites[i].launchDate, fetchedSatellites[i].orbitType, 
+             fetchedSatellites[i].operational);
+          this.sourceList.push(satellite);
+        }
+        this.displayList = this.sourceList.slice(0);
+     }.bind(this));
+  }.bind(this));
+
+}
+
+displayList: Satellite[];
+search(searchTerm: string): void {
+   let matchingSatellites: Satellite[] = [];
+   searchTerm = searchTerm.toLowerCase();
+   for(let i=0; i < this.sourceList.length; i++) {
+      let name = this.sourceList[i].name.toLowerCase();
+      let type = this.sourceList[i].type.toLowerCase();
+      let orbitType = this.sourceList[i].orbitType.toLowerCase();
+      if (name.indexOf(searchTerm) >= 0 || type.indexOf(searchTerm) >= 0 ||orbitType.indexOf(searchTerm) >= 0) {
+         matchingSatellites.push(this.sourceList[i]);
+      }
+   }
+   this.displayList = matchingSatellites;
+}
+
+
+ 
 }
